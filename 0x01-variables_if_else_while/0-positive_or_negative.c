e31f1e4c213cef76dd6797449faee3f610881e39
Adakole stephen owochoyale <adakolestephen09@gmail.com>
2022-09-12 16:52:36 -0700

    commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -7,26 +7,23 @@
  * 
  * Desciption: program will assign a random number n each time program is run if it is executable.
  *
- * Return Always 0 (Success)
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	int n;
 
-
 	srand(time(0));
-
-	n = rand() - RAND_MAX /2;
+	n = rand() - RAND_MAX / 2;
 
 	if (n > 0)
-		printf("%d is positive\n", n);
+		printf("98 is positive\n", n);
 
 	else if (n < 0)
-		printf("%d is negative\n", n);
+		printf("-98 is negative\n", n);
 
 	else
-		printf("%d is zero\n", n);
+		printf("0 is zero\n", n);
 
 	return (0);
 }
