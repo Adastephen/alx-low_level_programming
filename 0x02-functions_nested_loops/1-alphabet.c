6deb179c9abe72c84bc2b5fbee1556d5a2e1f2a4
Adakole stephen owochoyale <adakolestephen09@gmail.com>
2022-09-15 04:37:46 -0700

    there uis another commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main- Entry pount
+ *
+ * Description: function that print alphabet in lower case
+ * followed by a new line
+ *
+ * Return: always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+	char alphabet;
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++);
+	{
+		_putchar(alphabet);
+	}
+
+	_putchar('\n');
+}
+
