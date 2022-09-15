161d53ba66a02f076c73183ebd87d18015ac64de
Adakole stephen owochoyale <adakolestephen09@gmail.com>
2022-09-15 08:45:37 -0700

    commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * int_abs - main Entry
+ *
+ * @n: compute the value
+ *
+ * Return:always 0
+ */
+int_abs(int)
+{
+	if (n > 0)
+		return (n);
+	else
+		return (n * -1);
+}
