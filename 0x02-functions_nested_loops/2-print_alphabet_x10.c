54de02aa4e63d910a9c5f02c5e8ec33a228f4e15
Adakole stephen owochoyale <adakolestephen09@gmail.com>
2022-09-15 06:13:25 -0700

    this is the third commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * main- Entry point
+ *
+ * Description:funtion that print 10times,in lower
+ * case, followed by new line
+ * Retutn: always 0 (success)
+ */
+
+void print_alphabet_x10(void)
+{
+	int i = '0'
+	char c = 'a'
+
+	while (i <= '10')
+	{
+		while (c <= 'z')
+		{
+			_putchar(c);
+		}
+	_putchar('\n');
+	}
+
+}
