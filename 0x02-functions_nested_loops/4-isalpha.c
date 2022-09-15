5fd66a4c5880a0a6785f0d9e938d1cc92a370005
Adakole stephen owochoyale <adakolestephen09@gmail.com>
2022-09-15 08:05:02 -0700

    this is the fifth commit

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isalpha - main Entry point
+ *
+ * Description: this is to check if is in lowercase
+ * or uppercase letter
+ *
+ * Return: 1 if true , else return 0
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	return (0);
+}
