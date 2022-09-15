7d86a7a52bf96fcb97e25b27c1c5e8cf3fcd956a
Adakole stephen owochoyale <adakolestephen09@gmail.com>
2022-09-15 07:38:00 -0700

    this  is the  fourth commit

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main- Entry ppoint
+ *
+ * @c: character to compare
+ *
+ * Return: if true 1, else return 0
+ *
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	return (0);
+}
