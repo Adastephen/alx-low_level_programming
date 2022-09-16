fa933dc3f17bbed4fc78a20ae9bbf02fce91a4de
Adakole stephen owochoyale <adakolestephen09@gmail.com>
2022-09-15 23:31:08 -0700

    this is the commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -28,7 +28,7 @@ void times_table(void)
 				_putchar(' ');
 				_putchar(' ');
 				_putchar(k + '0');
-			{
+			}
 
 			else if (k >= 10)
 			{
@@ -40,4 +40,4 @@ void times_table(void)
 		}
 		_putchar('\n');
 	}
-		}
+}
