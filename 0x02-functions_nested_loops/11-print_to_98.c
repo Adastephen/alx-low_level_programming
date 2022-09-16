24acea6c4593746ecec7c10d5569a20d41db1b44
Adakole stephen owochoyale <adakolestephen09@gmail.com>
2022-09-16 06:49:50 -0700

    this is another commit

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,25 +12,16 @@ void print_to_98(int n)
 {	
 	if (n <= 98)
 	{
-		for (; n <= 98; n++)
+		for (n = n; n < 98; n++)
 		{
-			printf("%d", n);
-
-			if (n == 98)
-				continue;
-			printf(", ");
+			printf("%d, ", n);
+			printf("%d\n", 98);
 		}
-	}
-	else
-	{
-		for (; n >= 98; n--)
+		else
 		{
-			printf("%d", n);
-
-			if (n == 98)
-				continue;
-			printf(", ");
+			for (n = n; n > 98; n--)
+				printf("%d, ", n);
+				printf("%d\n", 98);
 		}
-		printf("\n");
 	}
 }
