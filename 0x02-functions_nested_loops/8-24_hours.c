cb8adb073833a866c24e09d53894332b8705f3e7
Adakole stephen owochoyale <adakolestephen09@gmail.com>
2022-09-15 17:13:20 -0700

    another commit

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * jack_bauer -main point
+ *
+ * Desccription: function that print every minute of the day
+ *
+ * Return: 1 else 0
+ */
+void jack_bauer(void)
+{
+	int M, H;
+
+	for (H = 0; H <= 24; H++)
+	{
+		for (M = 0; M <= 60; M++)
+		{
+			_putchar((H / 10) + '0');
+			_putchar((H % 10) + '0');
+			_putchar(':');
+			_putchar((M / 10) + '0');
+			_putchar((M % 10) + '0');
+			_putchar(':');
+			_putchar('\n');
+		}
+	}
+}
