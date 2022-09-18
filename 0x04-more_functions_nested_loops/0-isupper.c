6287cb4fb6378d9a0b5d89a4740e3769e03c4162
Adakole stephen owochoyale <adakolestephen09@gmail.com>
2022-09-18 15:43:12 -0700

    this is commit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _isupper - main entry
+ *
+ * @c: check the uppercase
+ * Return: 1 else 0
+ */
+int _isupper(int c)
+{
+	int (char c);
+
+	if (c == 'A')
+	{	_putchar(c);
+		_putchar("\n");
+
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,3 @@
+This is more nested loop
+
+The exercise will show how nested loop is important and valued to solve a problems 
