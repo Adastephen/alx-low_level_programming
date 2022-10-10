0e4cc4bc3523570d4372b7d05226672727f49c1c
Adakole stephen owochoyale <adakolestephen09@gmail.com>
2022-10-09 19:48:03 -0700

    more loops

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,12 +7,11 @@
  */
 void more_numbers(void)
 {
+	int i = '0';
 	int j;
 
-	for (j = "0"; j <= "10"; j++)
+	for (j = '0'; j <= '10'; j++)
 	{
-		int i = '0';
-
 		for (; i <= '14'; i++)
 		{
 			_putchar(i);
