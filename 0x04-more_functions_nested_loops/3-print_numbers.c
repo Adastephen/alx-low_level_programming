d918527b0ab5e8c96f7fed8dea9f5be0ff5b6599
Adakole stephen owochoyale <adakolestephen09@gmail.com>
2022-10-09 19:01:06 -0700

    one to ten

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,8 +2,7 @@
 
 /**
  * print_number - main function that print the number from 0 to 9
- *
- * @a: int a print 0 to 9
+ * followed by a new line
  * Return: Always 0
  */
 void print_numbers(void)
