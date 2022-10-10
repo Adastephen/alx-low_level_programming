00277a4967dd54a20c4df8b12d670bbbe69ee476
Adakole stephen owochoyale <adakolestephen09@gmail.com>
2022-10-10 16:41:34 -0700

    file name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - write a program that peint the name of the file
+ * it was compiled from,
+ * followed by a new line.
+ * Return: Always 0
+ */
+
+int main()
+{
+	char ch = '2-main.c';
+
+		_putchar(ch);
+	_putchar('\n');
+	return(0);
+}
