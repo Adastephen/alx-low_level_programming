ed888f4dc158d40cd609bfead917a8151c5691a0
Adakole stephen owochoyale <adakolestephen09@gmail.com>
2022-10-09 18:35:10 -0700

    this is another commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - main multiply two integer
+ *
+ * @a: int a
+ * @b: int b
+ * @c: multiply both int a and int b
+ * Return: Always 0
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
