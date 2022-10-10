8a27f2679c969f9ed13705144a08ccf25b4bf85d
Adakole stephen owochoyale <adakolestephen09@gmail.com>
2022-10-09 18:49:17 -0700

    another commmit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_number - main function that print the number from 0 to 9
+ *
+ * @a: int a print 0 to 9
+ * Return: Always 0
+ */
+void print_numbers(void)
+{
+	char ch = '0';
+
+	for (; ch <= '9'; ch++)
+	{
+		_putchar(ch);
+	}
+}
