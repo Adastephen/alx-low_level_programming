efd31451b742bf699334b758543b946b5e00a2f9
Adakole stephen owochoyale <adakolestephen09@gmail.com>
2022-10-21 04:18:14 -0700

    0x01 task

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Write a program that prints the alphabet in lowercase
+ * and then in uppercase
+ * followed by a new line.
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char ch = "a";
+	char uch = "A";
+
+	for (; ch <= "z"; ch++)
+	{
+		putchar(ch);
+	}
+	for (; uch <= "Z"; uch++)
+	{
+		putchar(uch);
+	}
+	putchar('\n');
+
+	return (0);
+}
